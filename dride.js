41ab8a2745f2776f3bd7baafa9838781dfbcf737
Yossi <saoron@gmail.com>
2016-08-22 15:42:43 +0300

    working dride prefix in cli

--- dride.js
+++ dride.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
 var path = require('path');
-var express = require('express');
 var pkg = require( path.join(__dirname, 'package.json') );
-
-
 
 // Parse command line options
 
@@ -15,21 +12,6 @@ program
   .option('-p, --port <port>', 'Port on which to listen to (defaults to 3000)', parseInt)
   .parse(process.argv);
 
-var port = program.port || 3000;
 
+console.log(' port: %j', program.port);
 
-// Ceate a new express app
-
-var app = express();
-
-// Serve static files from the frontend folder
-
-app.use('/', express.static(path.join(__dirname, 'frontend')));
-
-
-
-// Everything is setup. Listen on the port.
-
-app.listen(port);
-
-console.log('dride cli is running on port ' + port);
